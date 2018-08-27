aec2b0eaafee06f0b3ec2cdb559a326eecd52786
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-28 01:18:48 +0530

    assignment_1

---
+++ m1/Assignment-1/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+	public static void main(String[] args) {
+		String name= "Himaja";
+		int rollnumber= 20186028;
+		System.out.println("Name :" +name + "Roll Number :" + rollnumber);
+	}
+}
+	
