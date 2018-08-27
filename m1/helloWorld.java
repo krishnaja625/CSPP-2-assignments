d9c48f20098c05d6cbb587a577dbf311a75285d0
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-27 22:28:09 +0530

    helloworld

---
+++ m1/helloWorld.java
@@ -0,0 +1,15 @@
+/*first program
+*/
+final class helloWorld{
+	/*first class
+	*/	
+	private helloWorld(){ }
+	/**first class
+	*	@param args the command line
+	*/	
+	public static void main(String args[]){
+		System.out.println("Hello World");
+
+	}
+}
+
