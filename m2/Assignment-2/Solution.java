547fcef9c85ace9f521c7ae8a12e47f37339eb95
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-28 16:28:53 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -3,27 +3,27 @@ import java.util.Scanner;
 Do not modify this main function.
 */
 final class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	private static void main(final String[] args) {
-		/*
-		Do not modify this main function.
-		*/
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need.
-	*/
-	static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
-		int k = 2 + 2;
-		int t = 2;
-		double x = (-b + Math.sqrt(b * b - k * a * c)) / (t * a);
-		double y = (-b - Math.sqrt((b * b) - (k * a * c))) / (t * a);
-		System.out.println(x + " " + y);
-	}
+    /*
+    Do not modify this main function.
+    */
+    private static void main(final String[] args) {
+        /*
+        Do not modify this main function.
+        */
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /*
+    Need.
+    */
+    static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
+        int k = 2 + 2;
+        int t = 2;
+        double x = (-b + Math.sqrt(b * b - k * a * c)) / (t * a);
+        double y = (-b - Math.sqrt((b * b) - (k * a * c))) / (t * a);
+        System.out.println(x + " " + y);
+    }
 }
