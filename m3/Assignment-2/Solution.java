9f1b80ef00fe8948dc9240bce1e88a04d7c4cfa9
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-29 11:34:32 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-2/Solution.java
+++ m3/Assignment-2/Solution.java
@@ -11,15 +11,17 @@ public class Solution {
         int n = s.nextInt();
         int i = 0;
         int count = 0;
+        final int x = 10;
+        final int y = 7;
         int k = 0, in = 0;
     	for (i = 1; i <= n; i++) {
     		k = i;
     		while (k > 0) {
-    			in = k % 10;
-       		    if (in == 7) {
+    			in = k % x;
+       		    if (in == y) {
        		    	count++;
        			}
-            	k = k / 10;
+            	k = k / x;
         	}
         }
         System.out.println(count);
