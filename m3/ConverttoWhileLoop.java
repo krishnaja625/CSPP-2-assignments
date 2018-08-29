5e352b3d5570e0ebd5a5f73061be0bb74f12c687
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-29 10:31:21 +0530

    act 6

---
+++ m3/ConverttoWhileLoop.java
@@ -0,0 +1,22 @@
+final class ConverttoWhileLoop {
+/**
+*Do not modify this main function.
+*/
+    protected ConverttoWhileLoop() { }
+/**
+*Do not modify this main function.
+*@param args are command line arguments.
+*/
+    public static void main(final String[] args) {
+/**
+*Do not modify this main function.
+*/
+    int i = 2;
+    while (i <= 10) {
+        System.out.println(i);
+        i = i + 2;
+    }
+    System.out.println("GoodBye!");
+    }
+}
+
