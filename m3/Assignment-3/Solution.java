6ec1ea8c053f0e2af145cde58c3a517d7933dac6
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-29 12:08:34 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ m3/Assignment-3/Solution.java
@@ -0,0 +1,53 @@
+/**
+ * { item_description }.
+ */
+import java.util.Scanner;
+/**
+ * { item_description }.
+ */
+final class Solution {
+/**
+*Fill the main function to print the number of 7's between 1 to n.
+*/
+    protected Solution() { }
+/**
+ * { function_description }.
+ * @param      args  The arguments
+ */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner s=new Scanner(System.in);
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        int gcd = 0;
+        gcd(n1,n2);
+        System.out.println(gcd);
+    }
+    /*
+	Need to write the gcd function and print the output.
+	*/
+    static int gcd(int n1, int n2) {
+        int i = 1, j =1;
+        int num = 0;
+        int gcd = 0;
+        if (n1 > n2) {
+            num = n2;
+        }
+        else {
+            num = n1;
+        }
+        int a[] = new int[num];
+        for (i = 1; i <= num; i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                a[j] = i;
+                j = j + 1;
+            }
+        }
+        for (i = 1; i < j; i++) {
+            gcd = a[j] * gcd;
+        }
+        return gcd;
+    }
+}
