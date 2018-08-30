8a411e004337b591082962295e58a5d56b3a277e
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-30 12:13:23 +0530

    act 10

---
+++ practise/m4/FirstLast6.java
@@ -0,0 +1,51 @@
+/**
+ * { item_description }.
+ */
+import java.util.Scanner;
+/**
+ * { item_description }.
+ */
+final class FirstLast6 {
+/**
+*Fill the main function to print the number of 7's between 1 to n.
+*/
+    protected FirstLast6() { }
+/**
+ * { function_description }.
+ * @param      args  The arguments
+ */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }.
+         */
+        Scanner s = new Scanner(System.in);
+        int n= s.nextInt();
+        if (n >= 1) {
+            System.out.println(firstLast6(n));
+        }
+    }
+    /**
+     * { function_description }
+     *
+     * @param      n     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static boolean firstLast6(final int n) {
+        /**
+         * { var_description }
+         */
+        int i = 0;
+        int[] a;
+        a = new int[n];
+        Scanner scan = new Scanner(System.in);
+        for (i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+            }
+        if ( a[0] == 6 || a[n-1] == 6) {
+            return true;
+        }
+        return false;
+    }
+}
+
