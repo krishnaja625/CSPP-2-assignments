f7d2d2678199bcdb52fa2b4316987d954c3a2e89
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-30 14:23:42 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ assignments/m4/Assignment-2/Solution.java
@@ -0,0 +1,68 @@
+import java.util.*;
+/**
+ * Class for solution.
+ */
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    protected Solution() { }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int i = 0;
+        int j = 0;
+        int[][] a;
+        a = new int[m][n];
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+            }
+        int[][] b;
+        b = new int[m][n];
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                b[i][j] = sc.nextInt();
+            }
+            }
+        int[][] c;
+        c = new int[m][n];
+        c = summation(a, b, m, n);
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                System.out.println(c[i][j]);
+            }
+            }
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+
+    static int[][] summation(final int[][] a, final int[][] b, final int m, final int n) {
+        /**
+         * { var_description }
+         */
+        int c[][];
+        c = new int[m][n];
+        for (int i = 0; i < m; i++) {
+        	for (int j = 0; j < n; j++) {
+                c[i][j] = a[i][j] + b[i][j];
+        	}
+        }
+        return c;
+    }
+}
+
+
