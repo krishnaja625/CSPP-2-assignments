7145171d4176f1af0f0db45b0d9767b81557af33
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-31 16:02:46 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- assignments/m4/Assignment-4/Solution.java
+++ assignments/m4/Assignment-4/Solution.java
@@ -34,7 +34,7 @@ public class Solution {
     static String reverseString(final String s) {
         String str1 = "";
         for (int i = s.length() - 1; i >= 0; i--) {
-            str1 += s.charAt(i);
+            str1 = str1 + s.charAt(i);
         }
         return str1;
     }
