02eae8838adde0285f41b38629681a61eda76e0b
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-31 13:51:24 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- assignments/m4/Assignment-3/Solution.java
+++ assignments/m4/Assignment-3/Solution.java
@@ -1,7 +1,6 @@
 
 // Given an String, Write a java method that returns the decimal value for the given binary string.
 import java.util.Scanner;
-import java.lang.Math;
 public class Solution
 {/*
 	Do not modify this main function.
