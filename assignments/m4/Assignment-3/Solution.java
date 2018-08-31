ceb4b23b36c2bc6397b8e8b5996d6499dede0bac
krishnaja625 <krishnaja625@msitprogram.net>
2018-08-31 15:50:18 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- assignments/m4/Assignment-3/Solution.java
+++ assignments/m4/Assignment-3/Solution.java
@@ -22,10 +22,17 @@ public class Solution {
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             String s = sc.next();
-            String res=binaryToDecimal(s);
+            String res = binaryToDecimal(s);
             System.out.println(res);
         }
     }
+    /**
+     * { convertion of binary to decimal }
+     *
+     * @param      s     { s is of string type }
+     *
+     * @return     { the return type is string }
+     */
     static String binaryToDecimal(String s) {
         int sum = 0;
         int x = 1;
