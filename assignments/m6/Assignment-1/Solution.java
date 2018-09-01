ab9c3d4db8dd83d17283d8d6b05ef792bb482e5f
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-01 09:48:36 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- assignments/m6/Assignment-1/Solution.java
+++ assignments/m6/Assignment-1/Solution.java
@@ -18,18 +18,18 @@ public final class Solution {
      * @param      n     n value
      */
     static void oddComposites(final int n) {
-    	int c = 0;
-    	for(int i = 2;i <= n;i++){
-    		c = 0;
-    		for (int j = 1; j <= i; j++) {
-	    		if (i % j == 0) {
-	    			c++;
-	    		}
-    		}
-    		if (c != 2 & i % 2 != 0 ) {
-    			System.out.println(i);
-    		}
-    	}
+        int c = 0;
+        for (int i = 2;i <= n;i++){
+            c = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    c++;
+                }
+            }
+            if (c != 2 & i % 2 != 0 ) {
+                System.out.println(i);
+            }
+        }
     }
     /**
     * main method as driver program.
@@ -41,4 +41,3 @@ public final class Solution {
         oddComposites(n);
     }
 }
-
