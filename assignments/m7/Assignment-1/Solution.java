89fd8d0ac5863865c6a2e6619018d0a0c7c6c31d
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-03 18:44:48 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m7/Assignment-1/Solution.java
+++ assignments/m7/Assignment-1/Solution.java
@@ -8,28 +8,28 @@ import java.util.Scanner;
  * Class for input validator.
  */
 class InputValidator {
-	/**
-	 * The atributes and methods for InputValidator.
-	 */  
-	private String name; 
-	/**
-	 * Constructor initializes the person's name.
-	 * @param      name  The name
-	 */
-	public InputValidator(String name) {
+    /**
+     * The atributes and methods for InputValidator.
+     */
+    private String name;
+    /**
+     * Constructor initializes the person's name.
+     * @param      name  The name
+     */
+    public InputValidator(final String name) {
     this.name = name;
-	}
-	/**
-	 * Function to validate the data.
-	 * @return     { returns boolean type }
-	 */
-	boolean validateData() {
-		final int n = 6;
-		int k = name.length();
-		if (k >= n) {
-			return true;
-		}
-		return false;
+    }
+    /**
+     * Function to validate the data.
+     * @return     { returns boolean type }
+     */
+    boolean validateData() {
+        final int n = 6;
+        int k = name.length();
+        if (k >= n) {
+            return true;
+        }
+        return false;
 }
 }
 /**
@@ -39,11 +39,10 @@ public class Solution {
     /** { main funtion to perform matrix addition }.
      * @param      args  The arguments
      */
-	public static void main(String args[]) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData());
+    public static void main (final String args[]) {
+        Scanner s = new Scanner (System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);       
+        System.out.println (i.validateData());
     }
-
 }
