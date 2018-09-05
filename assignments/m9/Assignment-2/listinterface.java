d85ae9b3a2d811e2be7dedf6d7ab0bbf1eecbbaa
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-05 18:02:16 +0530

    assignment 2

---
+++ assignments/m9/Assignment-2/listinterface.java
@@ -0,0 +1,9 @@
+interface listinterface {
+	public void add(int item);
+	public int size();
+	public void remove(int index);
+	public int get(int index);
+	public String toString();
+	public boolean contains(int item);
+	public int indexOf(int item);
+}
