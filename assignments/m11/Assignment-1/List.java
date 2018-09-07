ccc41c34c6034bd82ea133e078a61695c806a85b
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-07 13:54:46 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- assignments/m11/Assignment-1/List.java
+++ assignments/m11/Assignment-1/List.java
@@ -230,9 +230,9 @@ public class List {
       */
      public void removeAll(int[] newArray)
      {
-        // write the logic 
-        for (int i= 0; i < size; i++) {
-            for (int j = 0; j < newArray.length; j++) {
+        // write the logic  
+        for (int j = 0; j < newArray.length; j++) {
+            for (int i= 0; i < size; i++) {
                 if (list[i] == newArray[j]) {
                     remove(i);
                 }
