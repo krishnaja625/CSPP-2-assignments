8620e55d2005e4f7b78b5209fbeff033ed8b723b
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-08 12:04:15 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- assignments/m13/Assignment-1/Solution.java
+++ assignments/m13/Assignment-1/Solution.java
@@ -106,13 +106,12 @@ class Set {
     public int[][] cartesianProduct(Set newset2) {
     	int rows = this.size() * newset2.size();
     	int[][] a = new int[rows][2];
-    	for (int i = 0; i < size();) {
+    	for (int i = 0; i < size(); i++) {
     		for (int j = 0; j < rows; j++) {
     			for (int k = 0; k < newset2.size(); k++) {
     			a[j][0] = this.set[i];
     			a[j][1] = newset2.set[k];
     			}
-    			i++;
     		}
     	}
     	return a;
