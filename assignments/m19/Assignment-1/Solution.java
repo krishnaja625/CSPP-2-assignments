acf243c1e7de21e79d1f4826664faa66b0cf0215
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-15 12:39:42 +0530

    "check_style_score -> 3 of 13 passed. style score: 0/1 "

--- assignments/m19/Assignment-1/Solution.java
+++ assignments/m19/Assignment-1/Solution.java
@@ -76,14 +76,14 @@ public final class Solution {
                     Integer.parseInt(data[4]), questionCount);
         	questionObject[i] = quiz;*/
 
-/*        for (int j = 0; j < data.length; j++) {
+        for (int j = 0; j < data.length; j++) {
         	if (data[j].equals("")) {
         		k = 1;
         		break;
         	}
-        }*/
+        }
     }
-        if (data.length != 5) {
+        if (k == 1) {
         	System.out.println("Error! Malformed question");
 
         } else if (questionCount == 0){
