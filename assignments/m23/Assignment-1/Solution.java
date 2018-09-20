fdb8e4121c2485c07c6200c8082a4be224736597
krishnaja625 <krishnaja625@msitprogram.net>
2018-09-20 20:45:55 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- assignments/m23/Assignment-1/Solution.java
+++ assignments/m23/Assignment-1/Solution.java
@@ -1,98 +1,80 @@
-import java.util.*;
-import java.lang.Math.*;
-import java.io.*;
-class Document {
-    String text1;
-    String text2;
-    Document() {
-        text1 = "";
-        text2 = "";
-    }
-    public static String DocumentToString(File doc) {
-        String fileToString = "";
-        try {
-            Scanner in = new Scanner(new FileReader(doc));
-            StringBuilder sb = new StringBuilder();
-            while (in.hasNext()) {
-                sb.append(in.next());
-                sb.append(" ");
-            }
-            in.close();
-            fileToString = sb.toString();
-        } catch(FileNotFoundException e) {
-            System.out.println("no file");
-        }
-        return fileToString;
-    }
-    public static Map removewords(String text) {
-        text = text.toLowerCase();
-        String[] words = text.replaceAll("[^a-zA-Z\\s ]", "").split(" ");
-        Map <String, Integer> map = new HashMap<>();
-        for (int i = 0; i  < words.length; i++) {
-            if(!map.containsKey(words[i])) {
-                map.put(words[i], 1);
-            } else {
-                map.put(words[i],map.get(words[i]) + 1);
-            }
-        }
-        // for (Map.Entry m:map.entrySet()) {
-        //  System.out.println(m.getKey() + " " + m.getValue());
-        // }
-        return map;
-    }
-    public static int compare(String textOne, String textTwo) {
-        float numerator = 0;
-        double denominator = 0;
-        float firstSum = 0;
-        float secondSum = 0;
-        Map <String,Integer> mapOne = removewords(textOne);
-        Map <String,Integer> mapTwo = removewords(textTwo);
-        for (String inmapOne : mapOne.keySet()) {
-            for (String inmapTwo : mapTwo.keySet()) {
-                if (inmapOne.equals(inmapTwo)) {
-                    numerator += mapOne.get(inmapOne) * mapTwo.get(inmapTwo);
-                }
-            }
-        }
-        for (String inmapOne : mapOne.keySet()) {
-            firstSum += Math.pow(mapOne.get(inmapOne),2) ;
-        }
-        for (String inmapTwo : mapTwo.keySet()) {
-            secondSum += Math.pow(mapTwo.get(inmapTwo),2);
-        }
-        denominator = Math.sqrt(firstSum) * Math.sqrt(secondSum);
-        double output = (numerator / denominator) * 100;
-        return  (int) ((output * 100D) / 100D) ;
-    }
-    
-}
-class Solution {
+import java.io.File; 
+import java.util.Scanner; 
+import java.util.Arrays;
+import java.util.HashMap;
+public class Solution 
+{ 
     Solution() {
 
     }
-    public static void main(String[] args) {
-        Document d = new Document();
-        String path;
+    public static void main(String[] args) throws Exception { 
         Scanner scan = new Scanner(System.in);
-        path = scan.nextLine();
-        File folder = new File(path);
-        File[] list = folder.listFiles();
-        for(File t : list) {
-            // System.out.println(t);
-        }
-        int length = list.length;
-        int[][] matrix = new int[length][length];
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                matrix[i][j] = d.compare(d.DocumentToString(list[i]),d.DocumentToString(list[j]));
+        String folder = scan.next();
+        File directory = new File(folder);
+        String[] file = directory.list();
+        String temp;
+        for(int i=0; i<file.length-1;i++) {
+            for(int j=0;j<file.length;j++) {
+                if(file[i].compareTo(file[j])>0) {
+                    temp = file[i];
+                    file[i] = file[j];
+                    file[j] = temp;
+                }
             }
         }
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
+        int[][] distance = new int[file.length][file.length];
+        for(int i=0;i<file.length;i++) {
+            for(int j=0;j<file.length;j++) {
+                File file1 = new File(folder+"\\"+file[i]);
+                File file2 = new File(folder+"\\"+file[j]);
+                HashMap<String, Integer> dict1 = getfile(new Scanner(file1));
+                HashMap<String, Integer> dict2 = getfile(new Scanner(file2));
+                distance[i][j] = similarity(dict1,dict2);
+
         }
-    
+        }
+/*    System.out.println(dict1);
+    System.out.println(dict2);*/
+    // similarity(dict1, dict2);
+}
+    static int similarity(HashMap<String, Integer> dict1, HashMap<String, Integer> dict2) {
+        double numerator = 0.0;
+        double denominator1 = 0.0;
+        double denominator2 = 0.0;
+        double result = 0.0;
+        for(String key : dict1.keySet()) {
+            if(dict2.containsKey(key)) {
+                numerator += dict1.get(key) * dict2.get(key);
+            }
+            }
+            for(String key : dict1.keySet()) {
+                int dict1value = dict1.get(key);
+                denominator1 += Math.pow(dict1value,2);
+            }
+            for(String key : dict2.keySet()) {
+                int dict2value = dict2.get(key);
+                denominator2 += Math.pow(dict2value,2);
+            }
+            result = numerator / (Math.sqrt(denominator1) * Math.sqrt(denominator2));
+            // System.out.println((int) (result*100));
+            return (int) result*100;
+    }
+    public static HashMap<String, Integer> getfile(Scanner sc) {
+        HashMap<String, Integer> words = new HashMap<String, Integer>();
+        while(sc.hasNext()) {
+            String line = sc.nextLine().toLowerCase();
+            line = line.replaceAll("[0-9_]","");
+            String[] wordlist =  line.split(" ");
+            for(int i=0;i<wordlist.length;i++) {
+                if(words.containsKey(wordlist[i])) {
+                    words.put(wordlist[i],words.get(wordlist[i])+1);
+                }
+                else {
+                    words.put(wordlist[i],1);
+                }
+            }
+        }
+        return words;
     }
 }
+    
